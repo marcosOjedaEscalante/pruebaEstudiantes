bd918198b483cb2555d40219235076c66bf52adb
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-08-07 20:57:58 -0400

    filtro completo

--- src/controllers/estudiantes.controller.js
+++ src/controllers/estudiantes.controller.js
@@ -1,6 +1,7 @@
 import { request, response } from "express";
 import { Estudiante } from "../models/estudiantes.js";
 import { Curso } from "../models/cursos.js";
+import { Op } from "sequelize";
 
 const findAll = async (req = request, res = response) => {
     try {
@@ -20,12 +21,28 @@ const findAll = async (req = request, res = response) => {
 }
 
 const findFilter = async (req = request, res = response) => {
-    const { idEstudiante } = req.body;
+    const { fecha_inicio_start = '1900-01-01', fecha_inicio_end = '2100-12-31', ...datos } = req.body;
+    const keys = Object.keys(datos);
+    const values = Object.values(datos);
+    const criteriosEstudiantes = {
+
+    }
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] !== null && values[i] !== undefined && values[i] !== '') {
+            criteriosEstudiantes[keys[i]] = values[i];
+        }
+    }
     try {
         const estudiantesFiltrados = await Estudiante.findAll({
-            where: {
-                'id_estudiante': idEstudiante
-            }
+            include: [{
+                model: Curso,
+                where: {
+                    fecha_inicio: {
+                        [Op.between]: [fecha_inicio_start, fecha_inicio_end]
+                    }
+                }
+            }],
+            where: criteriosEstudiantes
         });
         res.json({
             msg: 'funcionando finfilter',
